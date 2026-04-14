7e38bbe0f00668bc22e24fddad20caf5e1b1a6e2
박동현 <pdh0128a@gmail.com>
2026-04-14 09:00:25 +0900

    fix(Env): 환경변수 vault 주입

--- src/common/config/nova.py
+++ src/common/config/nova.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from pydantic_settings import BaseSettings
 
+from src.common.const.env import VAULT_ENV_FILE
+
 
 class NovaSettings(BaseSettings):
     bedrock_model_id: str = "global.amazon.nova-2-lite-v1:0"
@@ -10,4 +12,9 @@ class NovaSettings(BaseSettings):
     max_tokens: Optional[int] = None
     llm_api_key: Optional[str] = None
 
-    model_config = {"env_prefix": "NOVA_", "env_file": ".env", "extra": "ignore"}
+    model_config = {
+        "env_prefix": "NOVA_",
+        "env_file": VAULT_ENV_FILE,
+        "env_file_encoding": "utf-8",
+        "extra": "ignore",
+    }
